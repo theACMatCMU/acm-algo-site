0c862085d4678334b6a010cb66361578c2711f32
Vilcya Wirantana <vwirantana@gmail.com>
2013-10-16 19:41:32 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Acmalgo::Application.config.secret_token = '71f3490db2789f54ffb8064f5ade5d5b077451d5518e98c3a63e2e20a09e3d7f12ea91d3baf2f5ad855c7a454cc427378fc5e251837afb5606d0380c12653723'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Acmalgo::Application.config.session_store :cookie_store, key: '_acmalgo_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Acmalgo::Application.config.session_store :active_record_store
